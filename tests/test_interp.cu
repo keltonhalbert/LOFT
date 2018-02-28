ab38d8b2e2037fa6c150ba9b64f7b27d493041bf
keltonhalbert <keltonhalbert@ou.edu>
2018-02-28 21:35:45 +0000

    added test for _calc_weights

--- tests/test_interp.cu
+++ tests/test_interp.cu
@@ -2,7 +2,11 @@
 using namespace std;
 
 
-// this function tests the _nearest_grid_idx method in order 
+// This function tests the _nearest_grid_idx method in order 
+// to determine that things are working as intended.
+// It creates a small 30m isotropic grid and calls the GPU code
+// to find the nearest grid point index, and then returns the
+// grid point information for that index.
 __global__ void _testNearestIndex(float *point, float *nearest_point) {
 	printf("INPUT PARAMATERS\n");
 	printf("%0.6f %0.6f %0.6f\n", point[0], point[1], point[2]);
@@ -65,6 +69,44 @@ __global__ void _testNearestIndex(float *point, float *nearest_point) {
 
 }
 
+// Test whether or not the _calc_weights function works as intended.
+// Creates a small 30m isotropic grid to test the weigting function.
+// _calc_weights calls _nearest_grid_idx.
+__global__ void _testCalcWeights(float *point, float *weights_out) {
+
+	float *x_grd, *y_grd, *z_grd;
+	float *weights;
+	float pt_x, pt_y, pt_z;
+	int nX = 10;
+	int nY = 10;
+	int nZ = 10;
+
+	// initialize our fake 3D grid with a 30m grid spacing
+	// out 10 grid points in each direction
+	x_grd = new float[nX];
+	for (int i = 0; i < nX; i++) {
+		x_grd[i] = i*30.;
+	}
+
+	y_grd = new float[nY];
+	for(int j = 0; j < nY; j++) {
+		y_grd[j] = j*30.;
+	}
+
+	z_grd = new float[nZ];
+	for (int k = 0; k < nZ; k++) {
+		z_grd[k] = k*30.;
+	}
+
+	pt_x = point[0]; pt_y = point[1]; pt_z = point[2];
+
+	weights = _calc_weights(x_grd, y_grd, z_grd, pt_x, pt_y, pt_z, false, false, false, nX, nY, nZ);
+	for (int i = 0; i < 8; i++) {
+		weights_out[i] = weights[i];
+	}
+
+}
+
 
 void testNearestIndex() {
 	float *nearest_point;
@@ -103,11 +145,58 @@ void testNearestIndex() {
 	cout << "X input: " << point[0] << " | Nearest X Point: " << nearest_point[0] << endl;
 	cout << "Y input: " << point[1] << " | Nearest Y Point: " << nearest_point[1] << endl;
 	cout << "Z input: " << point[2] << " | Nearest Z Point: " << nearest_point[2] << endl << endl;
-	cout << "END NEAREST GRIDPOINT TEST" << endl << endl;
+	cout << "END NEAREST GRIDPOINT TEST" << endl << endl << endl << endl;
+}
+
+void testCalcWeights() {
+	float *point;
+	float *weights_out;
+	gpuErrchk( cudaMallocManaged(&point, 3 * sizeof(float)) );
+	gpuErrchk( cudaMallocManaged(&weights_out, 8 * sizeof(float)) );
+
+	for (int i = 0; i < 8; i++) {
+		weights_out[i] = -999.99;
+	}
+
+	cout << endl << "TESTING THE INTERPOLATION WEIGHTS CALCULATOR USING 30 METER ISOTROPIC" << endl;
+	cout << "CASE WHERE DATA IS EXACTLY ON GRID" << endl;
+	point[0] = 30.; point[1] = 30.; point[2] = 30.;
+	_testCalcWeights<<<1,1>>>(point, weights_out);
+	cudaDeviceSynchronize();
+	cout << "OUTPUT" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << "W" << i+1 << "=" << weights_out[i] << " ";
+	}
+	cout << endl << endl;
+
+	cout << "CASE WHERE DATA IS NOT ON GRID BUT IN GRID" << endl;
+	point[0] = 121.; point[1] = 65.; point[2] = 252.3337;
+	_testCalcWeights<<<1,1>>>(point, weights_out);
+	cudaDeviceSynchronize();
+	cout << "OUTPUT" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << "W" << i+1 << "=" << weights_out[i] << " ";
+	}
+	cout << endl << endl;
+
+	cout << "CASE WHERE DATA IS OUTSIDE OF THE DOMAIN" << endl;
+	point[0] = -100.; point[1] = 10000.; point[2] = 5000.;
+	_testCalcWeights<<<1,1>>>(point, weights_out);
+	cudaDeviceSynchronize();
+	cout << "OUTPUT" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << "W" << i+1 << "=" << weights_out[i] << " ";
+	}
+	cout << endl << endl;
+	cout << "END CALC WEIGHTS TEST" << endl << endl << endl << endl;
+
+
+
 }
 
 int main() {
 
 	testNearestIndex();
+	testCalcWeights();
 	return 0;
 }
