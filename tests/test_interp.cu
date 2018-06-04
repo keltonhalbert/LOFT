13b7b42d00b0bc464532a20308a4a46fdfb5df0f
keltonhalbert <kthalbert@wisc.edu>
2018-06-04 14:15:36 -0500

    added test for the triliniear interpolation and changed the other tests to reflect changes in the interpolation routines

--- tests/test_interp.cu
+++ tests/test_interp.cu
@@ -13,18 +13,13 @@ void _testNearestIndex(float *point, float *nearest_point) {
 	printf("%0.6f %0.6f %0.6f\n", point[0], point[1], point[2]);
 	printf("%0.6f %0.6f %0.6f\n\n", nearest_point[0], nearest_point[1], nearest_point[2]);
 
-	int *idx;
+	int idx[3];
 	float *x_grd, *y_grd, *z_grd;
 	float near_x, near_y, near_z;
-	float pt_x, pt_y, pt_z;
 	int pi, pj, pk;
 	int nX = 10;
 	int nY = 10;
 	int nZ = 10;
-
-	pt_x = point[0];
-	pt_y = point[1];
-	pt_z = point[2];
 
 	// initialize our fake 3D grid with a 30m grid spacing
 	// out 10 grid points in each direction
@@ -43,8 +38,9 @@ void _testNearestIndex(float *point, float *nearest_point) {
 		z_grd[k] = k*30.;
 	}
 
+
 	// get the nearest point index
-	idx = _nearest_grid_idx(pt_x, pt_y, pt_z, x_grd, y_grd, z_grd, nX, nY, nZ);
+	_nearest_grid_idx(point, x_grd, y_grd, z_grd, idx, nX, nY, nZ);
 	// use the index to get the nearest grid point values
 	pi= idx[0];
 	pj = idx[1];
@@ -76,8 +72,8 @@ void _testNearestIndex(float *point, float *nearest_point) {
 void _testCalcWeights(float *point, float *weights_out) {
 
 	float *x_grd, *y_grd, *z_grd;
-	float *weights;
-	float pt_x, pt_y, pt_z;
+	float weights[8];
+    int idx_3D[3];
 	int nX = 10;
 	int nY = 10;
 	int nZ = 10;
@@ -99,17 +95,61 @@ void _testCalcWeights(float *point, float *weights_out) {
 		z_grd[k] = k*30.;
 	}
 
-	pt_x = point[0]; pt_y = point[1]; pt_z = point[2];
 
 	// right now we just care about testing it on the unstaggered grid
 	// since we have a regular grid. Probably should test grid staggering
 	// at some point, but nah.
-	weights = _calc_weights(x_grd, y_grd, z_grd, pt_x, pt_y, pt_z, false, false, false, nX, nY, nZ);
+
+	// get the nearest point index
+	_nearest_grid_idx(point, x_grd, y_grd, z_grd, idx_3D, nX, nY, nZ);
+    _calc_weights(x_grd, y_grd, z_grd, weights, point, idx_3D,  false, false, false, nX, nY, nZ);
 	for (int i = 0; i < 8; i++) {
 		weights_out[i] = weights[i];
 	}
 
 }
+
+void _testTriInterp(float *point, float *interp_val) {
+
+    float *x_grd, *y_grd, *z_grd;
+    int nX = 10;
+    int nY = 10;
+    int nZ = 10;
+    int N = nX*nY*nZ;
+
+    float *data_arr = new float[N];
+
+	// initialize our fake 3D grid with a 30m grid spacing
+	// out 10 grid points in each direction
+	x_grd = new float[nX];
+	for (int i = 0; i < nX; i++) {
+		x_grd[i] = i*30.;
+	}
+
+	y_grd = new float[nY];
+	for(int j = 0; j < nY; j++) {
+		y_grd[j] = j*30.;
+	}
+
+	z_grd = new float[nZ];
+	for (int k = 0; k < nZ; k++) {
+		z_grd[k] = k*30.;
+	}
+
+    //initialize our fake data grid with ones in all dimensions
+    for (int idx = 0; idx < N; ++idx) {
+        data_arr[idx] = 1;
+    }
+
+    float result = interp3D(x_grd, y_grd, z_grd, data_arr, point, nX, nY, nZ);
+
+    cout << result << endl;
+    interp_val[0] = result; 
+
+    return;
+
+}
+//__host__ __device__ float _tri_interp(float* data_arr, int NX, int NY, float* weights, int i, int j, int k) {
 
 
 // The CPU method for testing the GPU method that returns the nearest grid index
@@ -198,9 +238,52 @@ void testCalcWeights() {
 
 }
 
+// the CPU method for testing the GPU method that returns the trilinear interpolation
+// weights for a regular 3D isotropic grid. 
+void testTriInterp() {
+	float *point = new float[3];
+	float *interp_val = new float[1];
+
+	// in this case, w1 should equal 1 and the other weights equal 0.
+	cout << endl << "TESTING THE INTERPOLATION CALCULATOR USING 30 METER ISOTROPIC" << endl;
+	cout << "CASE WHERE DATA IS EXACTLY ON GRID" << endl;
+	point[0] = 30.; point[1] = 30.; point[2] = 30.;
+	_testTriInterp(point, interp_val);
+	cout << "OUTPUT" << endl;
+	cout << "INTERP = " << interp_val[0];;
+	cout << endl << endl;
+
+    /*
+	// in this case, we should have 8 distinct weights with values < 1
+	cout << "CASE WHERE DATA IS NOT ON GRID BUT IN GRID" << endl;
+	point[0] = 121.; point[1] = 65.; point[2] = 252.3337;
+	_testCalcWeights(point, weights_out);
+	cout << "OUTPUT" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << "W" << i+1 << "=" << weights_out[i] << " ";
+	}
+	cout << endl << endl;
+
+	// in this case, all weights should return as -1 because 
+	// the requested point was outside of our specified domain
+	cout << "CASE WHERE DATA IS OUTSIDE OF THE DOMAIN" << endl;
+	point[0] = -100.; point[1] = 10000.; point[2] = 5000.;
+	_testCalcWeights(point, weights_out);
+	cout << "OUTPUT" << endl;
+	for (int i = 0; i < 8; i++) {
+		cout << "W" << i+1 << "=" << weights_out[i] << " ";
+	}
+	cout << endl << endl;
+	cout << "END CALC WEIGHTS TEST" << endl << endl << endl << endl;
+    */
+
+
+}
+
 int main() {
 
 	testNearestIndex();
 	testCalcWeights();
+    testTriInterp();
 	return 0;
 }
