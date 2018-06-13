3f77f9fd151ed154bbe47ec5d11c49dde491d00b
keltonhalbert <kthalbert@wisc.edu>
2018-06-12 22:31:10 -0500

    added cuda kernal

---
+++ integrate.cu
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+#ifndef INTEGRATE_CU
+#define INTEGRATE_CU
+
+// this is an error checking helper function for processes
+// that run on the GPU. Without calling this, the GPU can
+// fail to execute but the program won't crash or report it.
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      cout << cudaGetErrorString(code) << endl;
+      if (abort) exit(code);
+   }
+}
+
+// This is a GPU kernel for integrating a fluid parcel forward in time by increment dt.
+// It expects the X, Y, and Z array pointers as well as the U, V, and W wind component pointers,
+// which are all of length nParcels
+__global__ void integrate(float *x_arr, float *y_arr, float *z_arr, float *u_arr, float *v_arr, float *w_arr, int nParcels, float dt) {
+	// use the thread index to index the array
+	int pidx = blockIdx.x*blockDim.x + threadIdx.x;
+
+	// safety check to not access array memory out of bounds
+	if (pidx < nParcels) {
+		// integrate X position forward by the U wind
+		x_arr[pidx] = x_arr[pidx] + u_arr[pidx] * dt;
+		// integrate Y position forward by the V wind
+		y_arr[pidx] = y_arr[pidx] + v_arr[pidx] * dt;
+		// integrate Z position forward by the W wind
+		z_arr[pidx] = z_arr[pidx] + w_arr[pidx] * dt;
+	}
+}
+
+#endif
