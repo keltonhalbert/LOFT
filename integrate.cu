671531e8f0b3b51cd75b5753c4fc29733b29cde5
keltonhalbert <kthalbert@wisc.edu>
2018-06-13 18:28:14 -0500

    YESSSSSSSSSS MEMORY TRANSFERRED TO CUDA AND AN OPERATION IS CONDUCTED. HALLELUJAH

--- integrate.cu
+++ integrate.cu
@@ -35,9 +35,38 @@ __global__ void integrate(float *x_arr, float *y_arr, float *z_arr, float *u_arr
 	}
 }
 
+__global__ void test(float *u_time_chunk, float *v_time_chunk, float *w_time_chunk, int MX, int MY, int MZ, int nT) {
+    int N = MX*MY*MZ*nT;
+    float umax = -999.0;
+    float vmax = -999.0;
+    float wmax = -999.0;
 
+    for (int i = 0; i < N; ++i) {
+        if (u_time_chunk[i] > umax) umax = u_time_chunk[i];
+        if (v_time_chunk[i] > vmax) vmax = v_time_chunk[i];
+        if (w_time_chunk[i] > wmax) wmax = w_time_chunk[i];
+    }
+    printf("Umax: %f\tVmax: %f\tWmax: %f\n", umax, vmax, wmax);
+}
+
+/*This function handles allocating memory on the GPU, transferring the CPU
+arrays to GPU global memory, calling the integrate GPU kernel, and then
+updating the position vectors with the new stuff*/
 void cudaIntegrateParcels(parcel_pos parcels, float *u_time_chunk, float *v_time_chunk, float *w_time_chunk, int MX, int MY, int MZ, int nT) {
-    cout << "I MADE IT TO THE CUDA CODE!!!!" << endl;
+    // pointers to device memory
+    float *device_u_time_chunk, *device_v_time_chunk, *device_w_time_chunk;
+    // allocate the device memory
+    gpuErrchk( cudaMalloc(&device_u_time_chunk, MX*MY*MZ*nT*sizeof(float)) );
+    gpuErrchk( cudaMalloc(&device_v_time_chunk, MX*MY*MZ*nT*sizeof(float)) );
+    gpuErrchk( cudaMalloc(&device_w_time_chunk, MX*MY*MZ*nT*sizeof(float)) );
+    // copy the arrays to device memory
+    gpuErrchk( cudaMemcpy(device_u_time_chunk, u_time_chunk, MX*MY*MZ*nT*sizeof(float), cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpy(device_v_time_chunk, v_time_chunk, MX*MY*MZ*nT*sizeof(float), cudaMemcpyHostToDevice) );
+    gpuErrchk( cudaMemcpy(device_w_time_chunk, w_time_chunk, MX*MY*MZ*nT*sizeof(float), cudaMemcpyHostToDevice) );
+
+    test<<<1,1>>>(device_u_time_chunk, device_v_time_chunk, device_w_time_chunk, MX, MY, MZ, nT);
+    gpuErrchk( cudaDeviceSynchronize() );
+
 
 
 }
