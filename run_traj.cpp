fdf36ee01a6b21cd6f854dd9f6512f6110051da0
keltonhalbert <kthalbert@wisc.edu>
2018-06-25 10:16:58 -0500

    Adding code to dynamically request a grid from LOFS based on the parcel positions. Currently is broken though.

--- run_traj.cpp
+++ run_traj.cpp
@@ -6,39 +6,111 @@
 #include "datastructs.cpp"
 #include "integrate.h"
 
+// I made this myself by stealing from LOFS
+#define P2(t,p,mt) (((p)*(mt))+(t))
 // stole this define from LOFS
 #define P3(x,y,z,mx,my) (((z)*(mx)*(my))+((y)*(mx))+(x))
 // I made this myself by stealing from LOFS
 #define P4(x,y,z,t,mx,my,mz) ((t*mx*my*mz)+((z)*(mx)*(my))+((y)*(mx))+(x))
-// I made this myself by stealing from LOFS
-#define P2(t,p,mt) (((p)*(mt))+(t))
 using namespace std;
 
 
-/* Load the grid metadata and get the gird based on requested bounds.
- * This only needs to be called once to load data into memory. Additionally,
- * subsequent calls will look for cached metadata in .cm1hdf5_* files.
+/* Load the grid metadata and request a domain subset based on the 
+ * current parcel positioning for the current time step. The idea is that 
+ * for the first chunk of times read in (from 0 to N MPI ranks for time)
+ * only the subset of the domain that matters for that period of time. 
+ * When the next chunk of time is read in, check and see where the parcels
+ * are currently and request a subset that is relevent to those parcels.   
  */
-void loadMetadataAndGrid(string base_dir, datagrid *requested_grid) {
+void loadMetadataAndGrid(string base_dir, datagrid *requested_grid, parcel_pos *parcels, int tstep) {
 
     // query the dataset structure
     lofs_get_dataset_structure(base_dir);
     // get the HDF metadata - return the first filename
     get_hdf_metadata(firstfilename,&nx,&ny,&nz,&nodex,&nodey);
 
+    datagrid temp_grid;
+
     // for right now, set the grid bounds to the saved
     // bounds for testing purposes
-    requested_grid->X0 = saved_X0 + 180; requested_grid->Y0 = saved_Y0 + 180;
-    requested_grid->X1 = saved_X0 + 380; requested_grid->Y1 = saved_Y0 + 380;
-    requested_grid->Z0 = 0; requested_grid->Z1 = 100;
-    //requested_grid->X0 = saved_X0; requested_grid->Y0 = saved_Y0;
-    //requested_grid->X1 = saved_X1; requested_grid->Y1 = saved_Y1;
-    //requested_grid->Z0 = 0; requested_grid->Z1 = 379;
+    temp_grid.X0 = saved_X0; temp_grid.Y0 = saved_Y0;
+    temp_grid.X1 = saved_X1; temp_grid.Y1 = saved_Y1;
+    temp_grid.Z0 = 0; temp_grid.Z1 = nz-1; // nz comes from readlofs
 
-    // request a grid subset based on 
-    // the subset information provided to
-    // out grid struct
+    // request the full grid so that we can find the indices
+    // of where our parcels are, and then request a smaller
+    // subset from there.
+    lofs_get_grid(&temp_grid);
+
+    // find the min/max index bounds of 
+    // our parcels
+    float point[3];
+    int idx_4D[4];
+    int min_i = 999999;
+    int min_j = 999999;
+    int min_k = 999999;
+    int max_i = -1;
+    int max_j = -1;
+    int max_k = -1;
+    for (int pcl = 0; pcl < parcels->nParcels; ++pcl) {
+        point[0] = parcels->xpos[P2(tstep, pcl, parcels->nTimes)];
+        point[1] = parcels->ypos[P2(tstep, pcl, parcels->nTimes)];
+        point[2] = parcels->zpos[P2(tstep, pcl, parcels->nTimes)];
+        // find the nearest grid point!
+        _nearest_grid_idx(point, temp_grid.xh, temp_grid.yh, temp_grid.zh, idx_4D, temp_grid.NX, temp_grid.NY, temp_grid.NZ);
+        // check to see if we've found the min/max
+        // for the dimension
+        if (idx_4D[0] < min_i) min_i = idx_4D[0]; 
+        if (idx_4D[0] > max_i) max_i = idx_4D[0]; 
+        if (idx_4D[1] < min_j) min_j = idx_4D[1]; 
+        if (idx_4D[1] > max_j) max_j = idx_4D[1]; 
+        if (idx_4D[2] < min_k) min_k = idx_4D[2]; 
+        if (idx_4D[2] > max_k) max_k = idx_4D[2]; 
+    }
+
+    // we want to add a buffer to our dimensions so that
+    // the parcels don't accidentally move outside of our
+    // requested data. If the buffer goes outside the 
+    // saved dimensions, set it to the saved dimensions
+    min_i = saved_X0 + min_i - 10;
+    max_i = saved_X0 + max_i + 10;
+    min_j = saved_Y0 + min_j - 10;
+    max_j = saved_Y0 + max_j + 10;
+    min_k = min_k - 10;
+    max_k = max_k + 10;
+
+    // keep the data in our saved bounds
+    if (min_i < saved_X0) min_i = saved_X0;
+    if (max_i > saved_X1) max_i = saved_X1;
+    if (min_j < saved_Y0) min_i = saved_X0;
+    if (max_j > saved_Y1) max_i = saved_X1;
+    if (min_k < 0) min_k = 0;
+    if (max_k > nz-1) max_k = nz-1;
+
+
+
+    cout << "Parcel Bounds In Grid" << endl;
+    cout << "X0: " << min_i << " X1: " << max_i << endl;
+    cout << "Y0: " << min_j << " Y1: " << max_j << endl;
+    cout << "Z0: " << min_k << " Z1: " << max_k << endl;
+
+
+    requested_grid->X0 = min_i; requested_grid->Y0 = min_j;
+    requested_grid->X1 = max_i; requested_grid->Y1 = max_j;
+    requested_grid->Z0 = min_k; requested_grid->Z1 = max_k;
+
+    // request our grid subset now
+    cout << "REQUESTING METADATA & GRID" << endl;
     lofs_get_grid(requested_grid);
+    cout << "END METADATA & GRID REQUEST" << endl;
+    // clear the memory from the temp grid
+    delete[] temp_grid.xh;
+    delete[] temp_grid.yh;
+    delete[] temp_grid.zh;
+    delete[] temp_grid.xf;
+    delete[] temp_grid.yf;
+    delete[] temp_grid.zf;
+
 }
 
 /* Read in the U, V, and W vector components from the disk, provided previously allocated memory buffers
@@ -46,28 +118,54 @@ void loadMetadataAndGrid(string base_dir, datagrid *requested_grid) {
  */
 void loadVectorsFromDisk(datagrid *requested_grid, float *ubuffer, float *vbuffer, float *wbuffer, double t0) {
     // request 3D field!
-
-    lofs_read_3dvar(requested_grid, ubuffer, (char *)"uinterp", t0);
-    lofs_read_3dvar(requested_grid, vbuffer, (char *)"vinterp", t0);
-    lofs_read_3dvar(requested_grid, wbuffer, (char *)"winterp", t0);
+    lofs_read_3dvar(requested_grid, ubuffer, (char *)"u", t0);
+    lofs_read_3dvar(requested_grid, vbuffer, (char *)"v", t0);
+    lofs_read_3dvar(requested_grid, wbuffer, (char *)"w", t0);
 }
 
 
-/* Seed some test parcels into vectors of vectors for the different position dimension
+/* Seed some parcels into the domain
+ * in physical gridpoint space, and then
+ * fill the remainder of the parcel traces
+ * with missing values. 
  */
-void seed_parcels(parcel_pos *parcels, datagrid *requested_grid) {
+void seed_parcels(parcel_pos *parcels, int nTotTimes) {
+    // place a cube of parcels in the domain between xstart, 
+    // xend, ystart, yend, zstart, and zend in spacing
+    // increments dx, dy, dz
+    float xstart = -8000; float xend = -6500; float dx = 30;
+    float ystart = -3500; float yend = -2000; float dy = 30;
+    float zstart = 15; float zend = 1015; float dz = 30;
+    // the number of parcels we will be seeding
+    // - note I kind of made this by trial and error. 
+    // It may be subject to seg faults?
+    int nParcels = (int) ( (int)(((zend - zstart) + dz)/dz - 1) * (int)(((yend - ystart)+dy)/dy - 1) * (int)(((xend - xstart) + dx)/dx - 1));
+    cout << "SEEDING " << nParcels << " PARCELS" << endl;
 
-    int nParcels = parcels->nParcels;
+    // allocate memory for the parcels
+    // we are integrating for the entirety 
+    // of the simulation.
+    parcels->xpos = new float[nParcels * nTotTimes];
+    parcels->ypos = new float[nParcels * nTotTimes];
+    parcels->zpos = new float[nParcels * nTotTimes];
+    parcels->nParcels = nParcels;
+    parcels->nTimes = nTotTimes;
+
     int pid = 0;
-    for (int i = 30; i < 100; ++i) {
-        for (int j = 100; j < 170; ++j) {
-            parcels->xpos[P2(0, pid, parcels->nTimes)] = requested_grid->xh[i];
-            parcels->ypos[P2(0, pid, parcels->nTimes)] = requested_grid->yh[j];
-            parcels->zpos[P2(0, pid, parcels->nTimes)] = 1005.;
-            pid += 1;
+    for (float i = xstart; i < xend; i+=dx) {
+        for (float j = ystart; j < yend; j+=dy) {
+            for (float k = zstart; k < zend; k += dz) {
+                parcels->xpos[P2(0, pid, parcels->nTimes)] = i;
+                parcels->ypos[P2(0, pid, parcels->nTimes)] = j;
+                parcels->zpos[P2(0, pid, parcels->nTimes)] = k;
+                pid += 1;
+            }
         }
     }
 
+    // fill the remaining portions of the array
+    // with the missing value flag for the future
+    // times that we haven't integrated to yet.
     for (int p = 0; p < nParcels; ++p) {
         for (int t = 1; t < parcels->nTimes; ++t) {
             parcels->xpos[P2(t, pid, parcels->nTimes)] = -99999.0;
@@ -75,6 +173,7 @@ void seed_parcels(parcel_pos *parcels, datagrid *requested_grid) {
             parcels->zpos[P2(t, pid, parcels->nTimes)] = -99999.0;
         }
     }
+    cout << "END PARCEL SEED" << endl;
 
 }
 
@@ -127,7 +226,7 @@ int main(int argc, char **argv ) {
     string base_dir = "/u/sciteam/halbert/project_bagm/khalbert/30m-every-time-step/3D";
     int rank, size;
     long N, MX, MY, MZ;
-    int nTimeChunks = 3;
+    int nTimeChunks = 1;
 
     // initialize a bunch of MPI stuff.
     // Rank tells you which process
@@ -148,32 +247,23 @@ int main(int argc, char **argv ) {
 
     // we're gonna make a test by creating a horizontal
     // and zonal line of parcels
-    int nParcels = 4900;
     parcel_pos parcels;
     datagrid requested_grid;
 
-    // allocate memory for the parcels
-    // we are integrating for the entirety 
-    // of the simulation.
-    parcels.xpos = new float[nParcels * nTotTimes];
-    parcels.ypos = new float[nParcels * nTotTimes];
-    parcels.zpos = new float[nParcels * nTotTimes];
-    parcels.nParcels = nParcels;
-    parcels.nTimes = nTotTimes;
 
     for (int tChunk = 0; tChunk < nTimeChunks; ++tChunk) {
-
-        // read in the metadata - later we will make
-        // the requested grid dynamic based on the
-        // parcel seeds
-        loadMetadataAndGrid(base_dir, &requested_grid); 
-
         // if this is the first chunk of time, seed the
         // parcel start locations
         if (tChunk == 0) {
             cout << "SEEDING PARCELS" << endl;
-            seed_parcels(&parcels, &requested_grid);
+            seed_parcels(&parcels, nTotTimes);
         }
+
+        // read in the metadata - later we will make
+        // the requested grid dynamic based on the
+        // parcel seeds
+        loadMetadataAndGrid(base_dir, &requested_grid, &parcels, tChunk*size); 
+
 
         // the number of grid points requested
         N = (requested_grid.NX)*(requested_grid.NY)*(requested_grid.NZ);
@@ -228,6 +318,7 @@ int main(int argc, char **argv ) {
         if (rank == 0) {
             // send to the GPU
             // comment out if you're running on XE node
+            int nParcels = parcels.nParcels;
             float *uparcels = new float[nParcels * nTotTimes];
             float *vparcels = new float[nParcels * nTotTimes];
             float *wparcels = new float[nParcels * nTotTimes];
