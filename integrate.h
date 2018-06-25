73efff41e5d42ff686174af71513232d6912e38b
keltonhalbert <kthalbert@wisc.edu>
2018-06-25 10:17:18 -0500

    Adding code to dynamically request a grid from LOFS based on the parcel positions. Currently is broken though.

--- integrate.h
+++ integrate.h
@@ -2,6 +2,9 @@
 #include "datastructs.cpp"
 #ifndef INTEGRATE_H
 #define INTEGRATE_H
+
+
+void _nearest_grid_idx(float *point, float *x_grd, float *y_grd, float *z_grd, int *idx_4D, int nX, int nY, int nZ);
 void cudaIntegrateParcels(datagrid grid, parcel_pos parcels, float *u_time_chunk, float *v_time_chunk, float *w_time_chunk,\
                             float *uparcels, float *vparcels, float *wparcels,  int MX, int MY, int MZ, int nT, int tChunk, int totTime);
 #endif
