4a366f2f24e7975837fa94269f010cd0297d0efc
keltonhalbert <kthalbert@wisc.edu>
2020-02-04 05:20:08 +0000

    I'm bad at git

--- macros.cpp
+++ macros.cpp
@@ -24,19 +24,12 @@ using namespace std;
 // OK being a bit clever here ... fun with macros. This will make the
 // // code a lot easier to compare to native CM1 Fortran90 code that we are
 // // copying anyway. I adopt TEM for his tem array, UA for ua etc.
-#define BUF4D(x,y,z,t)   buf0[P4(x  ,y  ,z,t,NX+2,NY+2,NZ+1)]
-#define TEM4D(x,y,z,t)   dum0[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
-#define  UA4D(x,y,z,t)  ustag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
-#define  VA4D(x,y,z,t)  vstag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
-#define  WA4D(x,y,z,t)  wstag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
+#define BUF4D(x,y,z,t) buf0[P4(x,y,z,t,NX+2,NY+2,NZ+1)]
+#define TEM4D(x,y,z,t) dum0[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
+#define  UA4D(x,y,z,t) ustag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
+#define  VA4D(x,y,z,t) vstag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
+#define  WA4D(x,y,z,t) wstag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
 #define  KM4D(x,y,z,t) kmstag[P4(x+1,y+1,z,t,NX+2,NY+2,NZ+1)]
-
-#define BUF(x,y,z)   buf0[P3(x  ,y  ,z,NX+2,NY+2)]
-#define TEM(x,y,z)   dum0[P3(x+1,y+1,z,NX+2,NY+2)]
-#define  UA(x,y,z)  ustag[P3(x+1,y+1,z,NX+2,NY+2)]
-#define  VA(x,y,z)  vstag[P3(x+1,y+1,z,NX+2,NY+2)]
-#define  WA(x,y,z)  wstag[P3(x+1,y+1,z,NX+2,NY+2)]
-#define  KM(x,y,z) kmstag[P3(x+1,y+1,z,NX+2,NY+2)]
 
 #define PCL(t,p,mt) (((p)*(mt))+(t))
 // stole this define from LOFS
