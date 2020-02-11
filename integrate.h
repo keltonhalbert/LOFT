94c417281dcb2ed1d6a85ec9da133cf9313476b3
keltonhalbert <kthalbert@wisc.edu>
2020-02-11 01:43:06 +0000

    I'm dumb at git so basically it looks like I created a bunch of files and deleted other

--- integrate.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "datastructs.h"
-#ifndef INTEGRATE_H
-#define INTEGRATE_H
-
-
-void _nearest_grid_idx(float *point, datagrid *grid, int *idx_4D);
-void cudaIntegrateParcels(datagrid *grid, model_data *data, parcel_pos *parcels, int nT, int totTime, int direct);
-#endif
