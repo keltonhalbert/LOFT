80cdb041678eb599c03bccce36d6a0552dbe3005
Leigh Orf <lorf@ssec.wisc.edu>
2023-12-13 13:47:42 -0600

    Update to build script

--- buildLOFT-Frontera.sh
+++ buildLOFT-Frontera.sh
@@ -2,9 +2,12 @@
 
 #NOTE! YOU MUST BE ON AN RTX FRONTERA NODE
 
-module load cuda/12.2
-module load hdf5/1.14.0
-module load netcdf/4.9.0
+#These need to be typed by hand to stick, so do that, and then 'module
+#save loft' so you can 'module restore loft' later.
+
+#module load cuda/12.2
+#module load hdf5/1.14.0
+#module load netcdf/4.9.0
 
 make clean
 make
